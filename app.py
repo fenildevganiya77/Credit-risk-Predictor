d1d0b2ed2c45ce0d56ef66f561ee1906cdc51f15
Fenil Devganiya <fenildevganiya77@gmail.com>
2026-07-03 15:58:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-model = joblib.load("model.pkl")
+model = joblib.load("model/model.pkl")
 
 st.set_page_config(
     page_title="Credit Risk Predictor",
